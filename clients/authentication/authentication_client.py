07366556c9572cd434baad9ccc61c98e8ea15744
So11ka <dghtidy@gmail.com>
2025-08-28 00:30:01 +0300

    Added structure

---
+++ clients/authentication/authentication_client.py
@@ -0,0 +1,19 @@
+from httpx import Response
+from clients.api_client import APIClient
+from typing import TypedDict
+
+class LoginRequestDict(TypedDict):
+    email: str
+    password: str
+
+class RefreshRequestDict(TypedDict):
+    refreshToken: str
+
+class AuthenticationClient(APIClient):
+
+    def login_api(self, request: LoginRequestDict) -> Response:
+        return self.post('localhost:8000/api/v1/authentication/login', json=request)
+
+    def refresh_api(self, request: RefreshRequestDict) -> Response:
+        return self.post('localhost:8000/api/v1/authentication/refresh', json=request)
+
