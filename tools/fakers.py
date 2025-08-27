a0e3e52b7946b74a6d4fc6abcd1c9c36bd7072a0
So11ka <dghtidy@gmail.com>
2025-08-27 10:57:01 +0300

    Added double checks

--- tools/fakers.py
+++ tools/fakers.py
@@ -5,11 +5,13 @@ import string
 def random_email() -> str:
     return f"user{int(time.time())}@example.com"
 
-def random_name() -> str:
-    return rnd.choice(('Jonh', 'Patricia', 'May', 'Elizabet', 'Victor', 'Dack'))
+def random_name(name: str=None) -> str:
+    names = ['Jonh', 'Patricia', 'May', 'Elizabet', 'Victor', 'Dack']
+    return rnd.choice(names) if name is None else rnd.choice([i for i in names if i != name])
 
-def random_surname() -> str:
-    return rnd.choice(('Parker', 'Norton', 'Adams', 'Watson', 'Murphy', 'Jackson'))
+def random_surname(surname: str=None) -> str:
+    surnames = ['Parker', 'Norton', 'Adams', 'Watson', 'Murphy', 'Jackson']
+    return rnd.choice(surnames) if surname is None else rnd.choice([i for i in surnames if i != surname])
 
 def random_password(len_pass=12) -> str:
     result = string.ascii_letters + string.digits
