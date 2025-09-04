62a3b48a34de633a51047707ef64638a881da21e
So11ka <dghtidy@gmail.com>
2025-09-04 15:41:37 +0300

    Написание API автотестов используя Pytest. Практика написания API автотестов

--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -3,7 +3,7 @@ from tools.assertions.base import assert_equal
 
 
 def assert_create_user_response(request: CreateUserRequestSchema, response: UserResponseSchema):
-    assert_equal(response.email, request.user.email, 'email')
-    assert_equal(response.first_name, request.user.first_name, 'first_name')
-    assert_equal(response.middle_name, request.user.middle_name, 'middle_name')
-    assert_equal(response.last_name, request.user.last_name, 'last_name')
+    assert_equal(request.email, response.user.email, 'email')
+    assert_equal(request.first_name, response.user.first_name, 'first_name')
+    assert_equal(request.middle_name, response.user.middle_name, 'middle_name')
+    assert_equal(request.last_name, response.user.last_name, 'last_name')
