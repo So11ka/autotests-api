29bb633958261a32436265e9d02a946fd5f3ef02
So11ka <dghtidy@gmail.com>
2025-09-11 11:06:48 +0300

    cURL в Allure-отчете: упрощаем анализ API-тестов

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -4,6 +4,8 @@ from httpx import Client
 from clients.authentication.authentication_client import AuthenticationClient
 from pydantic import BaseModel
 from clients.authentication.authentication_schema import LoginRequestSchema
+from clients.event_hooks import curl_event_hook
+
 
 class AuthenticationRequestSchema(BaseModel, frozen=True):
     """
@@ -31,4 +33,8 @@ def get_private_http_client(data: AuthenticationRequestSchema) -> Client:
     # Вызываем метод аутентификации и передаем в него словарь с email, password
     login_response = authentication_client.get_login(request_login)
 
-    return Client(timeout=10, base_url='http://localhost:8000', headers={'Authorization': f'Bearer {login_response.token.access_token}'})
+    return Client(timeout=10,
+                  base_url='http://localhost:8000',
+                  headers={'Authorization': f'Bearer {login_response.token.access_token}'},
+                  event_hooks={'request': [curl_event_hook]}
+    )
--- clients/public_http_builder.py
+++ clients/public_http_builder.py
@@ -1,5 +1,8 @@
 from httpx import Client
 
+from clients.event_hooks import curl_event_hook
+
+
 def get_public_http_client(timeout: int | float) -> Client:
     """
     Метод для возврата не клиента
@@ -7,4 +10,7 @@ def get_public_http_client(timeout: int | float) -> Client:
     :param timeout: Количество времени отведенного на запрос
     :return: Ответ от сервера в виде объекта httpx.Client
     """
-    return Client(base_url='http://localhost:8000', timeout=timeout)
+    return Client(base_url='http://localhost:8000',
+                  timeout=timeout,
+                  event_hooks={'request': [curl_event_hook]}
+    )
