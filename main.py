741da2fbf1078325dfa1be8e176b15d7016ab0fc
So11ka <dghtidy@gmail.com>
2025-09-15 10:38:51 +0300

    Added run autotests on CI

--- main.py
+++
@@ -1,7 +0,0 @@
-import httpx
-
-payload = {"email": "user34@example.com", "password": "qwer1234"}
-
-response1 = httpx.post('http://localhost:8000/api/v1/authentication/login', json=payload, timeout=10)
-response2 = httpx.post('http://localhost:8000/api/v1/authentication/refresh', json={'refreshToken': response1.json()['token']['refreshToken']}, timeout=10)
-print(response2.json())
